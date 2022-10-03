7aabc0c0a07071ae8a77b01695cedc0dec569fb9
WalentynKarpiuk <walenty.karpiuk@gmail.com>
2022-10-03 15:40:07 +0200

    Adding loop

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,20 @@
 package org.example;
 
+
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println("Podaj czas do wybuchu");
+        Scanner scan = new Scanner(System.in);
+        int i = scan.nextInt();
+        for (int j = i; j >=0 ; j--) {
+            System.out.println("Launch for:" + j);
+            Thread.sleep(500);
+        }
+
     }
+    // This step is need for creating new branch lesson 1
+
 }
